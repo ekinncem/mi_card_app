befe8b692f1b3e4b74c979d33c100d1cfe78f97b
ekinncem <ekinn.ciem16@gmail.com>
2024-12-06 03:18:17 +0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 
@@ -7,40 +7,86 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('images/myphoto.png'),
-              ),
-              Text(
-                "Ekin Cem Falcioglu",
-                style: TextStyle(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('images/myphoto.png'),
+                ),
+                Text(
+                  "Ekin Cem Falcioglu",
+                  style: TextStyle(
                     fontSize: 30.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'Ballet'),
-              ),
-              Text("Flutter Developer",
+                    fontFamily: 'Ballet',
+                  ),
+                ),
+                Text(
+                  "Flutter Developer",
                   style: TextStyle(
-                      fontSize: 20,
-                      color: const Color.fromARGB(255, 49, 46, 46),
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Quicksand',
-                      letterSpacing: 3.5)),
-            ],
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 49, 46, 46),
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Quicksand',
+                    letterSpacing: 3.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      '+90 530 447 21 06',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Quicksand',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.mail,
+                        color: Colors.teal.shade900,
+                      ),
+                      title: Text(
+                        'ekinn.ciem16@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Quicksand',
+                          fontSize: 20,
+                        ),
+                      ),
+                    )),
+              ],
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
